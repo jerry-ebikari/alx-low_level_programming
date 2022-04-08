e680ef374eb8b6ad9b536ed98cc0dc8632863c44
jerry-ebikari <jerryebikarineedam@gmail.com>
2022-04-08 14:24:17 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 int character = 97;
 
 while (character < 123)
-if (character++ && character != 101 && character != 113)
+if (character++ && character != 102 && character != 114)
 putchar(character - 1);
 putchar('\n');
 return (0);
