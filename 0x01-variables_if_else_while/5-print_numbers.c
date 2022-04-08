994bb4ae6870430ca276f9f6fa7e6a556c112a7d
jerry-ebikari <jerryebikarineedam@gmail.com>
2022-04-08 14:34:54 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return - 0 for success
+ */
+
+int main(void)
+{
+int number = 0;
+
+while (number < 10)
+printf("%d", number++);
+putchar('\n');
+return (0);
+}
