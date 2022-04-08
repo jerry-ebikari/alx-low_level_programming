8a6ae147873ce06db2b0bc3281623f7fde154c8b
jerry-ebikari <jerryebikarineedam@gmail.com>
2022-04-08 13:28:19 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Should return 0
+ */
+
+int main(void)
+{
+int character = 97;
+
+while (character < 123)
+putchar(character++);
+return (0);
+}
