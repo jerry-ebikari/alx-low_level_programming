23573228b318420ec698f87e533860a22a2e0138
Jerry Ebikari Needam <71222695+jerry-ebikari@users.noreply.github.com>
2022-04-09 02:18:32 +0100

    Update 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: Should be 0
+ **/
+int main( )
+{
+int c11 = 48;
+int c12 = 48;
+int c21 = 48;
+int c22 = 49;
+
+while (c11 < 58)
+{
+c12 = 48;
+while (c12 < 58)
+{
+if ((c21 >= 58 || c11 == c21) && c22 >=58)
+{
+c21 = c11;
+c22 = c12 + 1;
+}
+else
+c21 = 48;
+while (c21 < 58)
+{
+if (c11 != c21)
+c22 = 48;
+while (c22 < 58)
+{
+putchar(c11);
+putchar(c12);
+putchar(' ');
+putchar(c21);
+putchar(c22);
+if (!(c11 == 57 && c12 == 56))
+{
+putchar(',');
+putchar(' ');
+}
+c22++;
+}
+c21++;
+}
+c12++;
+}
+c11++;
+}
+putchar('\n');
+return (0);
+}
